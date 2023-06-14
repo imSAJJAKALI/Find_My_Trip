253ab1934249eb3ee0677b4d13fbc3ee39b8839c
imSAJJAKALI <sajjak2506@gmail.com>
2023-06-14 11:57:52 +0530

    day-1

--- find-my-trip/src/App.tsx
+++ find-my-trip/src/App.tsx
@@ -12,9 +12,10 @@ import Navbar from "./Components/Navbar"
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
-      
+      <Navbar/>
       
     </Box>
+    <h1>Neelesh</h1>
     <h3>its Working........</h3>
   </ChakraProvider>
 )
