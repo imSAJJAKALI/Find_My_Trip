d86bbb32cd34b25edf2b8a2aed1ee8c6a2128aed
imSAJJAKALI <sajjak2506@gmail.com>
2023-06-17 15:35:27 +0530

    day-4

--- find-my-trip/src/Pages/mainRoute.tsx
+++ find-my-trip/src/Pages/mainRoute.tsx
@@ -7,6 +7,7 @@ import Reviews from './Reviews'
 import ContactUs from './ContactUs'
 import Navbar from '../Components/Navbar'
 import LoginPage from './Login'
+import SignupPage from './SignUp'
 
 export default function MainRoute() {
   return (
@@ -17,9 +18,10 @@ export default function MainRoute() {
       <Route path='/destinations' element={<Destinations/>}/>
       <Route path='/booking' element={<Booking/>}/>
       <Route path='/reviews' element={<Reviews/>}/>
-      <Route path='/contact-Us' element={<ContactUs/>}/>
+      <Route path='/ContactUs' element={<ContactUs/>}/>
       
       <Route path='/login' element={<LoginPage/>}/>
+      <Route path='/signup' element={<SignupPage/>}/>
       
 
     </Routes>
